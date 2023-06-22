914775871b3466b1fdf6bbbab0bdceaefb6cfd07
Jason Feng <jasonfen@usc.edu>
2023-06-22 03:36:29 -0700

    init

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException, Response
+import secrets
+import pickle
+import os
+
+app = FastAPI()
+
+def load_urls():
+    if os.path.exists('urls.pkl'):
+        with open('urls.pkl', 'rb') as f:
+            return pickle.load(f)
+    else:
+        return {}
+
+def save_urls(urls):
+    with open('urls.pkl', 'wb') as f:
+        pickle.dump(urls, f)
+
+@app.get("/{short_url}")
+async def read_url(short_url: str):
+    urls = load_urls()
+    url = urls.get(short_url)
+    if url is None:
+        raise HTTPException(status_code=404, detail="URL not found")
+    response = Response(headers={"Location": url})
+    response.status_code = 303
+    return response
+
+@app.post("/shorten/")
+async def shorten_url(url: str):
+    short_url = secrets.token_hex(3)  # Generate a random 6-character hex string
+    urls = load_urls()
+    while short_url in urls:
+        short_url = secrets.token_hex(3)  # Regenerate if the URL is already in use
+    urls[short_url] = url
+    save_urls(urls)
+    return {"short_url": short_url}
